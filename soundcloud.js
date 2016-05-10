45392d622672f88f22850a37a5d79ecb7ffb5b4b
Noah Scholfield <contact@noahscholfield.com>
2016-05-10 00:34:57 -0400

    Change description to html, add links, title, username

--- soundcloud.js
+++ soundcloud.js
@@ -1,15 +1,17 @@
-var app = angular.module("soundcloud", [])
+var app = angular.module("soundcloud", ['ngSanitize'])
 var scapi = "https://crossorigin.me/https://api.soundcloud.com/resolve.json?url="
 var client = "&client_id=30cba84d4693746b0a2fbc0649b2e42c"
 
 app.controller("descriptionController", ["$http", function($http) {
   var sc = this
   sc.showJSON = false
+  sc.html = []
   sc.submit = function() {
     var callURL = scapi + sc.url + client
     $http.get(callURL)
       .success(function(response) {
         sc.trackJSON = response
+        sc.html = JSONtoHTML(response.description)
       })
       .error(function(response) {
         console.log(response)
@@ -19,3 +21,27 @@ app.controller("descriptionController", ["$http", function($http) {
     sc.showJSON = !sc.showJSON
   }
 }])
+
+var JSONtoHTML = function(string) {
+  this.HTML = string.split('\n')
+  this.HTML.forEach(function(item, index, array) {
+    if(item == '') {
+      array[index] = '<br>'
+    } else if(item.indexOf('http') !== -1) {
+      array[index] = addATags(item)
+    }
+  })
+  return this.HTML
+}
+
+var addATags = function(string) {
+  this.linkStart = string.indexOf('http')
+  this.beforeLink = string.substring(0, this.linkStart)
+  this.link = string.substring(this.linkStart)
+  this.rest = ''
+  if(this.link.indexOf(' ') !== -1) {
+    this.rest = this.link.substring(this.link.indexOf(' '))
+    this.link = this.link.substring(0, this.link.indexOf(' '))
+  }
+  return this.beforeLink + '<a target="_blank" href=\"' + this.link + '\">' + this.link + '</a>' + this.rest
+}
