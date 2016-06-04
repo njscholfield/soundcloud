9cabfd67c24d57707ae6671ab97ef024e235d7ab
Noah Scholfield <contact@noahscholfield.com>
2016-06-04 16:51:20 -0400

    Tweak JSONP error message

--- soundcloud.js
+++ soundcloud.js
@@ -21,7 +21,7 @@ app.controller("descriptionController", ["$http", function($http) {
             .then(function success(response) {
               processJSON(response)
             }, function error(response) {
-              sc.trackJSON = {"error": "Something went wrong... This could have been caused by a track for which the information is not available. Please try again.", "code": "'JSONP Response Code' + response.status"}
+              sc.trackJSON = {"error": "Something went wrong... This could have been caused by a track for which the information is not available or a server/network problem. Please try again.", "code": "JSONP Response Code " + response.status}
             })
         }
         console.log(response.status + ' ' + response.statusText)
